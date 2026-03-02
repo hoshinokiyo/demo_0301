a1470906c37f881dd24584c4f2ee7ab17c999757
LAPTOP-DCG2OSMM\academia <202511_aw_hoshino@x-ide.sakura.ne.jp>
2026-03-02 11:20:15 +0900

    Section 8-3 完了状態の切り替え

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -71,6 +71,12 @@ public class TodoController {
         return "redirect:/todo";
     }
 
+    @PostMapping("/{id}/toggle")
+    public String toggle(@PathVariable("id") Long id) {
+        todoService.toggleCompleted(id);
+        return "redirect:/todo";
+    }
+
     @PostMapping("/{id}/delete")
     public String delete(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
         if (todoService.deleteById(id)) {
